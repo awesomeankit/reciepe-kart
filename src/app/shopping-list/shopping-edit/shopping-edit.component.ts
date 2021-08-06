4a83c444c6f35231c9eb77ede79f8c4097133141
awesomeankit <ankitsawant40@gmail.com>
2021-08-07 02:58:46 +0530

    Added navigation, recipe-details select and shopping-list add using Components and DataBinding

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingridient } from 'src/app/shared/ingridient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild("nameInput", {static: false})
+  nameInputRef: ElementRef;
+  @ViewChild("amountInput", {static: false})  
+  amountInputRef: ElementRef;
+  @Output()
+  ingridientAdded= new EventEmitter<Ingridient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddIngridient(){
+    this.ingridientAdded.emit(new Ingridient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    ));
   }
 
 }
